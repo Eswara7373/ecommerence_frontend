1c9267b767b1fe14005667b0a6f1e3ce8fe86753
Eswar Prasad <eswarprasad7373@gmail.com>
2023-03-03 01:00:04 +0530

    first commit

---
+++ src/pages/insiderouing/AllPage.jsx
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react'
+import "../../index.css"
+import Items from '../../components/Items'
+// import { allpageData } from '../../datas/provider/insidepagedatas/allpage'
+
+const AllPage = () => {
+
+  const[allpageData,setallpageData]=useState("");
+
+  useEffect(()=>{
+    fetch("https://ecommerence-backend-eswar.vercel.app/allpageData")
+    //https://ecommerence-backend-eswar.vercel.app/
+    .then(res=>res.json())
+    .then(json=>setallpageData(json))
+    .catch(err=>console.log("error"))
+  },[])
+
+  return (
+    <>
+    <div className='bestseller'>
+      {
+        allpageData && allpageData.map((val)=>{
+          return(
+            <>
+              <Items key={val.id} img={val.img} rating={val.rating} name={val.name} original={val.original} offer={val.offer}/>
+            </>
+          ) 
+        })
+      }
+    </div>
+    </>
+  )
+}
+
+export default AllPage
